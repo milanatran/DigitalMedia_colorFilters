82d632356e039cfe51b1b3e9bc3d389a72029827
MILANA TRAN <MILANATRAN.NTM@gmail.com>
2021-05-25 20:06:17 +0200

    Added exercise + solutions for a few tasks

---
+++ GLDM_U3_s0577618.java
@@ -0,0 +1,358 @@
+package uebung3;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.gui.ImageCanvas;
+import ij.gui.ImageWindow;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+     Opens an image window and adds a panel below the image
+ */
+public class GLDM_U3_s0577618 implements PlugIn {
+
+	ImagePlus imp; // ImagePlus object
+	private int[] origPixels;
+	private int width;
+	private int height;
+
+	String[] items = {"Original", "Rot-Kanal", "Negativ", "Graustufen", "Binärbild", "Graustufen 5", "Graustufen 10", "Binär mit horiz. Fehlerdiff.", "Sepia", "6 Farben"};
+
+
+	public static void main(String args[]) {
+
+		IJ.open("https://imi-gdm.github.io/uebungen/uebung3/Bear.jpg");
+		//IJ.open("Z:/Pictures/Beispielbilder/orchid.jpg");
+
+		GLDM_U3_s0577618 pw = new GLDM_U3_s0577618();
+		pw.imp = IJ.getImage();
+		pw.run("");
+	}
+
+	public void run(String arg) {
+		if (imp==null) 
+			imp = WindowManager.getCurrentImage();
+		if (imp==null) {
+			return;
+		}
+		CustomCanvas cc = new CustomCanvas(imp);
+
+		storePixelValues(imp.getProcessor());
+
+		new CustomWindow(imp, cc);
+	}
+
+
+	private void storePixelValues(ImageProcessor ip) {
+		width = ip.getWidth();
+		height = ip.getHeight();
+
+		origPixels = ((int []) ip.getPixels()).clone();
+	}
+
+
+	class CustomCanvas extends ImageCanvas {
+
+		CustomCanvas(ImagePlus imp) {
+			super(imp);
+		}
+
+	} // CustomCanvas inner class
+
+
+	class CustomWindow extends ImageWindow implements ItemListener {
+
+		private String method;
+		
+		CustomWindow(ImagePlus imp, ImageCanvas ic) {
+			super(imp, ic);
+			addPanel();
+		}
+
+		void addPanel() {
+			//JPanel panel = new JPanel();
+			Panel panel = new Panel();
+
+			JComboBox cb = new JComboBox(items);
+			panel.add(cb);
+			cb.addItemListener(this);
+
+			add(panel);
+			pack();
+		}
+
+		public void itemStateChanged(ItemEvent evt) {
+
+			// Get the affected item
+			Object item = evt.getItem();
+
+			if (evt.getStateChange() == ItemEvent.SELECTED) {
+				System.out.println("Selected: " + item.toString());
+				method = item.toString();
+				changePixelValues(imp.getProcessor());
+				imp.updateAndDraw();
+			} 
+
+		}
+
+
+		private void changePixelValues(ImageProcessor ip) {
+
+			// Array zum Zurückschreiben der Pixelwerte
+			int[] pixels = (int[])ip.getPixels();
+
+			if (method.equals("Original")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						
+						pixels[pos] = origPixels[pos];
+					}
+				}
+			}
+			
+			if (method.equals("Rot-Kanal")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						//int g = (argb >>  8) & 0xff;
+						//int b =  argb        & 0xff;
+
+						int rn = r;
+						int gn = 0;
+						int bn = 0;
+
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}
+			
+			if (method.equals("Negativ")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >>  8) & 0xff;
+						int b =  argb        & 0xff;
+						
+						int rn = 255 - r;
+						int gn = 255 - g;
+						int bn = 255 - b;
+
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}
+			
+			if (method.equals("Graustufen")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >>  8) & 0xff;
+						int b =  argb        & 0xff;
+						
+						int d = (r+g+b)/3; //Durchschnittswert
+						int rn = d;
+						int gn = d;
+						int bn = d;
+
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}
+			
+			if (method.equals("Binärbild")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >>  8) & 0xff;
+						int b =  argb        & 0xff;
+						
+						int d = (r+g+b)/3; //Durchschnittswert
+						int rn = d;
+						int gn = d;
+						int bn = d;
+						
+						int bw = 255 / 2; //mit dem int entscheiden ob Pixel schwarz wird oder weiß
+						
+						if(d < bw) {
+							rn = 0;
+							gn = 0;
+							bn = 0;
+						} else {
+							rn = 255;
+							gn = 255;
+							bn = 255;
+						}
+
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}
+			
+			/*if (method.equals("Graustufen 5")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >>  8) & 0xff;
+						int b =  argb        & 0xff;
+						
+						//int stufen = 255 / 5; // in 5 Graustufen unterteilen
+						//double m = (r+g+b)/3; //Durchschnitt
+						
+						//double d = m/stufen; //Durchschnittswert
+						int d = (r+g+b)/3;
+						if(d <= 51) {
+							d = 0;
+						} else if(d <= 51 * 2) {
+							d = 51;
+						} else if(d <= 51*3) {
+							d = 51 * 2;
+						} else if(d <= 51 * 4) {
+							d = 51 * 3;
+						} else {
+							d = 255;
+						}
+						int rn = d;
+						int gn = d;
+						int bn = d;
+
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}*/
+			
+			
+			if (method.equals("Graustufen 5")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >>  8) & 0xff;
+						int b =  argb        & 0xff;
+						
+						int m = (r+g+b)/3; //Durchschnitt
+						int stufen = 255/5; //in 5 Graustufen geteilt
+						int d = m / stufen * stufen;
+						int rn = d;
+						int gn = d;
+						int bn = d;
+
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}
+			
+			
+			if (method.equals("Graustufen 10")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >>  8) & 0xff;
+						int b =  argb        & 0xff;
+						
+						int m = (r+g+b)/3; // Durchschnitt
+						int stufen = 255/10; //in 10 Graustufen geteilt
+						int d = m / stufen * stufen;
+						int rn = d;
+						int gn = d;
+						int bn = d;
+
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}
+			
+			//Quelle: https://www.tutorialspoint.com/how-to-convert-a-colored-image-to-sepia-image-using-java-opencv-libraryhttps://www.tutorialspoint.com/how-to-convert-a-colored-image-to-sepia-image-using-java-opencv-library
+			if (method.equals("Sepia")) {
+
+				for (int y=0; y<height; y++) {
+					for (int x=0; x<width; x++) {
+						int pos = y*width + x;
+						int argb = origPixels[pos];  // Lesen der Originalwerte 
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >>  8) & 0xff;
+						int b =  argb        & 0xff;
+						
+						int sepia = 20;
+						int d = (r+g+b)/3; //Durchschnittswert
+						int rn = d + (sepia * 2);
+						int gn = d + sepia;
+						int bn = d - 30;
+						
+						if(rn > 255) rn = 255;
+						if(gn > 255) gn = 255;
+						if(bn > 255) bn = 255;
+						if(bn < 0) bn = 0;
+						// Hier muessen die neuen RGB-Werte wieder auf den Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF<<24) | (rn<<16) | (gn<<8) | bn;
+					}
+				}
+			}
+
+	} // CustomWindow inner class
+		
+		
+	} 
+}
